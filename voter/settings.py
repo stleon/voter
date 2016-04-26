7e85b2370a82ac648cc0d43f17516342fc39cdf3
STLEON <leonst998@gmail.com>
2016-04-26 23:52:47 +0300

    extra fields

--- voter/settings.py
+++ voter/settings.py
@@ -95,7 +95,8 @@ STATIC_URL = '/static/'
 # VK
 
 AUTHENTICATION_BACKENDS = (
-    'social.backends.vk.VKOAuth2',
+    # 'social.backends.vk.VKOAuth2',
+    'casting.backends.CustomVKOAuth',
     'django.contrib.auth.backends.ModelBackend',
 )
 
@@ -104,3 +105,4 @@ SOCIAL_AUTH_VK_OAUTH2_KEY = os.getenv('VK_OAUTH2_KEY')
 SOCIAL_AUTH_VK_OAUTH2_SECRET = os.getenv('VK_OAUTH2_SECRET')
 SOCIAL_AUTH_VK_APP_USER_MODE = 2
 SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['photos', 'offline']
+SOCIAL_AUTH_EXTRA_DATA = ['sex', 'photo_200'] # https://vk.com/dev/fields
