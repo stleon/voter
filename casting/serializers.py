cbf60c8461747296a56461b79420564fb49b73a6
STLEON <leonst998@gmail.com>
2016-05-08 00:36:09 +0300

    top serializator

--- casting/serializers.py
+++ casting/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from .models import CastingUser
@@ -7,6 +8,21 @@ class CastingUserSerializer(serializers.ModelSerializer):
     """
     Сериализатор модели участника голосования.
     """
+
     class Meta:
         model = CastingUser
         exclude = ('id', )
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', )
+
+
+class TopSerializer(CastingUserSerializer):
+    info = UserSerializer(source='user')
+    stars = serializers.FloatField(label="Звезды")
+
+    class Meta(CastingUserSerializer.Meta):
+        pass
