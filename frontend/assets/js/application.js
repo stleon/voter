1a7a9c7df80655253ea360d14b679d5fe72e7c7a
Mike Khromov <m.hromov@afisha.ru>
2016-05-23 19:24:27 +0300

    add front

---
+++ frontend/assets/js/application.js
@@ -0,0 +1,19 @@
+'https://api.vk.com/method/photos.get?owner_id=' + 'VKID' + '&album_id=profile' // VKID from API GET photo user
+
+'/casting-users/id' // - ID USER 
+
+'/choices/' // - list POST
+'/choices/id' // + uuid (data) from /choices/ PUT
+
+
+'/casting-users/top/' // for list.html
+
+'/casting-users/1/' //
+
+// $.ajax({
+//   url: '/casting-users/1/',
+//   data: 'http://new-new-photo.jpg',
+//   success: function (data) {
+
+//   }
+// })
