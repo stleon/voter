92575f0e552ae3e0afce3c2c3d53383780307a8a
Belousow Makc <lib.bmw@gmail.com>
2016-05-24 16:29:11 +0300

    Update static_root

--- voter/settings.py
+++ voter/settings.py
@@ -91,8 +91,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
